647b04310892cfe199c9b8ee29036905bd1c3002
Mapi <matvey2000000009@gmail.com>
2025-08-25 00:28:59 +0400

    Main sniff functionality done

---
+++ Bot/DataConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace PhotonPackageParser;
+
+public static class DataConverter
+{
+    public static void ConvertRawData()
+    {
+        long conversionFactor = 10000;
+        
+        string fileName = "out.json";
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+        
+        JArray marketData = JArray.Parse(File.ReadAllText(path));
+
+        var maxPerItem = marketData
+            .Where(tok => tok["ItemTypeId"] != null && tok["UnitPriceSilver"] != null && (int)tok["QualityLevel"] < 4)
+            .GroupBy(tok => (string)tok["ItemTypeId"])
+            .ToDictionary(
+                g => g.Key,
+                g => {
+                    long maxRaw = g
+                        .Select(x => {
+                            var v = x["UnitPriceSilver"];
+                            if (v == null) return 0L;
+                            try { return (long) v; } catch {}
+                            try { return Convert.ToInt64((string)v); } catch {}
+                            try { return Convert.ToInt64((double)v); } catch {}
+                            return 0L;
+                        })
+                        .Max();
+                    return maxRaw / conversionFactor;
+                }
+            );
+
+        // Convert to JObject for pretty JSON output
+        var resultObj = JObject.FromObject(maxPerItem);
+
+        File.WriteAllText("max_prices_by_item.json", resultObj.ToString());
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PhotonPackageParser;
+using SharpPcap;
+
+CaptureManager sniffer = new CaptureManager(device: CaptureDeviceList.Instance[3], "out");
+sniffer.Start();
+Console.ReadLine();      // <- keeps process alive
+sniffer.Stop();
+
+DataConverter.ConvertRawData();
